0d30b15aeea33088c6eca4f237fe75c153d2b2b2
caydenferguson <caydenferguson@172.29.185.177>
2019-01-15 21:01:46 -0500

    Removed my GameBoard class to easily merge with @MissStrong's completed version

--- CheckersGame/src/checkersgame/CheckersGame.java
+++ CheckersGame/src/checkersgame/CheckersGame.java
@@ -18,7 +18,7 @@ public class CheckersGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new GameBoard().setVisible(true);
+        
     }
     
 }
